c56b36486ce0486613a94f3efc24c755beb8fe44
takewiki <hulilei@takewiki.com.cn>
2019-10-13 12:13:11 +0800

    move the funcs from nscl into nsclpkg

--- R/cl_aux_funcs.R
+++ R/cl_aux_funcs.R
@@ -1,3 +1,4 @@
+# 1. 判断是否为最新的语料-------
 #' 判断是否为最新的语料
 #'
 #' @param FBrand 品牌
@@ -25,3 +26,48 @@ is_new_cl_data <- function(FBrand,FDate){
   }
   return(res);
 }
+
+
+# 取得原始语料的最大行--------
+#' 取得原始语料的最大行
+#'
+#' @return 返回值
+#' @import tsda
+#' @export
+#'
+#' @examples
+#' max_cl_id()
+max_cl_id <- function(){
+
+  res <- nsim_max_id('im_raw_record','FId');
+
+  return(res);
+}
+
+
+
+
+
+#  处理语料数据保存-------
+#' 处理语料数据保存问题
+#'
+#' @param data 数据
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' cl_write_data();
+cl_write_data <- function(data){
+  ncount <- nrow(data);
+  conn_nsim <- tsda::conn_nsim();
+  data$FId <- max_cl_id()+1:ncount;
+  data$FNeedRBind<-rep(0,ncount);
+  data$FRbindId <- rep(0,ncount);
+  data <-data[,c('FId','FUser','FDatetime','FLog','FDate','FTime','FBrand','FNeedRBind','FRbindId')]
+  tsda::db_writeTable(conn=conn_nsim,table_name = 'im_raw_record',r_object = data,append = T)
+
+
+}
+
+
--- R/qalist.R
+++ R/qalist.R
@@ -1,3 +1,4 @@
+# 判断是否为最新的QA对信息------
 #' 判断是否为最新的QA对信息
 #'
 #' @param FBrand  品牌
@@ -23,4 +24,38 @@ is_new_qalist <- function(FBrand,FDate){
     res <-FALSE
   }
   return(res);
+}
+
+# 获取QAList的最大码------
+#' 获取QAList的最大码
+#'
+#' @return 返回值
+#' @import tsda
+#' @export
+#'
+#' @examples
+#' max_qa_id
+max_qa_id <- function(){
+  #处理方式非常漂亮！ 2019-10-13
+  res <- nsim_max_id('qalist_raw','FSessionId')
+  return(res);
+}
+
+# 保存QA数据------
+#' 保存QA数据
+#'
+#' @param data 数据
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' qa_write_data();
+qa_write_data <- function(data){
+  ncount <- nrow(data);
+  conn_nsim <- tsda::conn_nsim()
+  data$FSessionId <- max_qa_id()+1:ncount;
+  data <-data[,c("FBrand","FDate","FGroupId","FSessionId","FQuestionText","FAnswerText")]
+  tsda::db_writeTable(conn=conn_nsim,table_name = 'qalist_raw',r_object = data,append = T)
+
 }
